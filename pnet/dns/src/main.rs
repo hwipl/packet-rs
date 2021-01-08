b0497e85951a41d076fea6e3d2e87662445fd087
hwipl <33433250+hwipl@users.noreply.github.com>
2021-01-08 16:37:22 +0100

    pnet/dns: add initial dns packet parsing
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pnet/dns/src/main.rs
+++ pnet/dns/src/main.rs
@@ -1,11 +1,44 @@
 use std::convert::TryInto;
 
 use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::Packet;
 use pnet::transport::TransportChannelType::Layer4;
 use pnet::transport::TransportProtocol::Ipv4;
 use pnet::transport::{transport_channel, udp_packet_iter};
 
+const DNS_HEADER_LENGTH: usize = 12;
 const DNS_PORT: u16 = 53;
+
+// dns packet consists of the following 16 bit fields:
+//
+// Identification,
+// Flags,
+// Number of questions,
+// Number of answers,
+// Number of authority resource records (RRs)
+// Number of additional RRs
+//
+// use methods to read fields from the packet
+struct DnsPacket<'a> {
+    raw: &'a [u8],
+}
+
+impl<'a> DnsPacket<'a> {
+    // create a new dns packet from raw packet bytes
+    pub fn new(raw: &'a [u8]) -> Option<DnsPacket<'a>> {
+        if raw.len() < DNS_HEADER_LENGTH {
+            println!("short dns packet with length {}", raw.len());
+            None
+        } else {
+            Some(DnsPacket { raw: raw })
+        }
+    }
+
+    // get identification field from packet
+    pub fn get_id(&self) -> u16 {
+        read_be_u16(&self.raw[0..2])
+    }
+}
 
 // convert a 16 bit field from big endian to native byte order
 fn read_be_u16(bytes: &[u8]) -> u16 {
@@ -32,7 +65,16 @@ fn main() {
                 if packet.get_source() != DNS_PORT && packet.get_destination() != DNS_PORT {
                     continue;
                 }
-                println!("got dns packet from {}", addr);
+
+                // parse dns packet
+                let dns = match DnsPacket::new(packet.payload()) {
+                    Some(dns) => dns,
+                    None => {
+                        println!("malformed dns packet");
+                        continue;
+                    }
+                };
+                println!("got dns packet from {} with id {}", addr, dns.get_id());
             }
             Err(e) => {
                 panic!("An error occurred while reading: {}", e);
