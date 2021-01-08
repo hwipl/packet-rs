f56728ac18892b36ad4118235ab9a67207fde857
hwipl <33433250+hwipl@users.noreply.github.com>
2021-01-08 16:35:47 +0100

    pnet/dns: add helper read_be_u16()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pnet/dns/src/main.rs
+++ pnet/dns/src/main.rs
@@ -1,9 +1,16 @@
+use std::convert::TryInto;
+
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::transport::TransportChannelType::Layer4;
 use pnet::transport::TransportProtocol::Ipv4;
 use pnet::transport::{transport_channel, udp_packet_iter};
 
 const DNS_PORT: u16 = 53;
+
+// convert a 16 bit field from big endian to native byte order
+fn read_be_u16(bytes: &[u8]) -> u16 {
+    u16::from_be_bytes(bytes.try_into().expect("slice with incorrect length"))
+}
 
 fn main() {
     // create an udp channel
