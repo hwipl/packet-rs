ff2c9bc32e8f9af94bfd153616e9c12177a95cdb
hwipl <33433250+hwipl@users.noreply.github.com>
2021-01-08 15:26:42 +0100

    pnet/dns: add dns packet capturing
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pnet/dns/src/main.rs
+++ pnet/dns/src/main.rs
@@ -1,3 +1,33 @@
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::transport::TransportChannelType::Layer4;
+use pnet::transport::TransportProtocol::Ipv4;
+use pnet::transport::{transport_channel, udp_packet_iter};
+
 fn main() {
-    println!("Hello, world!");
+    // create an udp channel
+    let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Udp));
+    let (_, mut rx) = match transport_channel(4096, protocol) {
+        Ok((tx, rx)) => (tx, rx),
+        Err(e) => panic!(
+            "An error occurred when creating the transport channel: {}",
+            e
+        ),
+    };
+
+    // read udp packets from channel and handle dns packets
+    let mut iter = udp_packet_iter(&mut rx);
+    loop {
+        match iter.next() {
+            Ok((packet, addr)) => {
+                // only handle dns packets
+                if packet.get_source() != 53 && packet.get_destination() != 53 {
+                    continue;
+                }
+                println!("got dns packet from {}", addr);
+            }
+            Err(e) => {
+                panic!("An error occurred while reading: {}", e);
+            }
+        }
+    }
 }
