ed085825d48b176163dc7488f7cc540b8888d1ee
hwipl <33433250+hwipl@users.noreply.github.com>
2021-01-10 12:54:52 +0100

    pnet/dns: use get_name() in print_name()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pnet/dns/src/main.rs
+++ pnet/dns/src/main.rs
@@ -81,19 +81,7 @@ impl<'a> DnsQuestion<'a> {
 
     // get the dns name from raw packet bytes
     pub fn print_name(&self) {
-        print!("Domain name: ");
-        for i in &self.label_indexes {
-            // get length of current label from first byte
-            let length: usize = usize::from(self.raw[*i]);
-
-            // TODO: check if current label is a reference to another one
-
-            // read domain name part from current label
-            let j = i + 1;
-            let part = str::from_utf8(&self.raw[j..j + length]).unwrap();
-            print!("{}.", part);
-        }
-        println!("");
+        println!("Domain name: {}", self.get_name());
     }
 
     // get the name field from raw packet bytes
