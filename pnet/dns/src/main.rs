bd72fde09aaaef394096a58b8f386ebf0ae1ab47
hwipl <33433250+hwipl@users.noreply.github.com>
2021-01-12 13:13:18 +0100

    pnet/dns: add helper function read_be_u32()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pnet/dns/src/main.rs
+++ pnet/dns/src/main.rs
@@ -380,6 +380,11 @@ fn read_be_u16(bytes: &[u8]) -> u16 {
     u16::from_be_bytes(bytes.try_into().expect("slice with incorrect length"))
 }
 
+// convert a 32 bit field from big endian to native byte order
+fn read_be_u32(bytes: &[u8]) -> u32 {
+    u32::from_be_bytes(bytes.try_into().expect("slice with incorrect length"))
+}
+
 fn main() {
     // create an udp channel
     let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Udp));
