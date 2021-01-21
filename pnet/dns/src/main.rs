7e725c9b55ce2115c2a5715acbcbb1d38f5fdc2f
hwipl <33433250+hwipl@users.noreply.github.com>
2021-01-21 18:57:36 +0100

    pnet/dns: add helper function read_be_u128()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pnet/dns/src/main.rs
+++ pnet/dns/src/main.rs
@@ -912,6 +912,11 @@ fn read_be_u32(bytes: &[u8]) -> u32 {
     u32::from_be_bytes(bytes.try_into().expect("slice with incorrect length"))
 }
 
+// convert a 128 bit field from big endian to native byte order
+fn read_be_u128(bytes: &[u8]) -> u128 {
+    u128::from_be_bytes(bytes.try_into().expect("slice with incorrect length"))
+}
+
 fn main() {
     // create an udp channel
     let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Udp));
